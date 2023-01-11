17c8daba88bad09254532af97fd229131fc8af62
杜磊 <61581722@qq.com>
2023-01-11 11:19:03 +0800

    feature(下载简历): 修正代码

--- src/utils/zipdownload.js
+++ src/utils/zipdownload.js
@@ -38,12 +38,12 @@ export function resolveBlob(res, mimeType) {
   const aLink = document.createElement('a')
   const blob = new Blob([res.data], { type: mimeType })
   // //从response的headers中获取filename, 后端response.setHeader("Content-disposition", "attachment; filename=xxxx.docx") 设置的文件名;
-  const regExp = new RegExp('filename=([^;]+\\.[^\\.;]+);*')
+  const regExp = new RegExp('filename=([^;]+\\.[^.;]+);*')
   const contentDisposition = decodeURI(res.headers.get('content-disposition') || res.headers.get('Content-Disposition'))
   console.log(contentDisposition)
   const result = regExp.exec(contentDisposition)
   let fileName = result[1]
-  fileName = fileName.replace(/\"/g, '')
+  fileName = fileName.replace(/"/g, '')
   aLink.href = URL.createObjectURL(blob)
   aLink.setAttribute('download', fileName) // 设置下载文件名称
   document.body.appendChild(aLink)
