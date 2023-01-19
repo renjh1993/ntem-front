d1ca8582bd3cc0d27afff081e77850adf6241707
zhangluqing <2283987177@qq.com>
2023-01-19 09:06:49 +0800

    员工管理添加菜单导入功能

--- src/views/yggl/leave/index.vue
+++ src/views/yggl/leave/index.vue
@@ -1679,7 +1679,7 @@ export default {
         // 设置上传的请求头部
         headers: { Authorization: 'Bearer ' + getToken() },
         // 上传的地址
-        url: process.env.VUE_APP_BASE_API + '/system/user/importData'
+        url: process.env.VUE_APP_BASE_API + '/emuser/tEmUser/importData'
       },
       // 表单参数
       // form: {
